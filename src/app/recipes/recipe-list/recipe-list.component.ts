2db9aaa3c9db3ecfccad1f118a3324424cb3407b
Tim Kuntze <tim@MacBook-Pro-von-Tim.local>
2022-03-28 17:54:12 +0200

    added components and basic html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description', 'https://runningonrealfood.com/wp-content/uploads/2021/01/Vegan-Slow-Cooker-Red-Lentil-Chili-Recipe-5-700x1049.jpg'),
+    new Recipe('Test Recipe', 'Test Description', 'https://runningonrealfood.com/wp-content/uploads/2021/01/Vegan-Slow-Cooker-Red-Lentil-Chili-Recipe-5-700x1049.jpg')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingedient } from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingedients: Ingedient[] = [
+    new Ingedient ('Apples', 5),
+    new Ingedient ('Tomatoes', 10)
+  ];
 
   constructor() { }
 
